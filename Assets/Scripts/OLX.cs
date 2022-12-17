e06ed5bf23ead2bdce25f40bbb74ceba2959e2b0
Slain505 <vladyslav.yefimenko@protonmail.com>
2022-12-17 21:42:01 +0100

    my interesting experiments 🤓

--- Assets/Scripts/OLX.cs
+++ Assets/Scripts/OLX.cs
@@ -12,7 +12,8 @@ namespace DefaultNamespace
                 age = 10,
                 country = "Italy",
                 isCar = true,
-                hp = 5.2,
+                hp = 5.2f,
+                price = 10900.50,
             };
             
             var Horse1 = new Vehicle
@@ -22,10 +23,14 @@ namespace DefaultNamespace
                 country = "Poland",
                 hp = .9f,
                 isCar = false,
+                price = 1499.99,
             };
 
-            var horse1LeftForSale = Horse1.daysLeftForSell();
-            var PorscheLeftForSale = Porsche.daysLeftForSell();
+            var horse1LeftForSale = Horse1.DaysLeftForSell();
+            var porscheLeftForSale = Porsche.DaysLeftForSell();
+
+            var horse1PriceOnSite = Horse1.SalePrice();
+            var porschePriceOnSite = Porsche.SalePrice();
         }
     }
 }
--- Assets/Scripts/Vehicle.cs
+++ Assets/Scripts/Vehicle.cs
@@ -10,21 +10,37 @@ namespace DefaultNamespace
         public int age;
         public string country;
         public bool isCar;
-        public double hp;
+        public float hp;
         public int countForSale;
+        public double price;
 
         #endregion
 
         #region Methods
 
         //Meth-ods
-        public int daysLeftForSell()
+        public int DaysLeftForSell()
         {
             // return 15;
             countForSale = age - 5;
             return countForSale;
         }
 
+        private double Discount()
+        {
+            if (age <= 10)
+            {
+                price = price * 0.2f;
+            }
+
+            return price;
+        }
+
+        public double SalePrice()
+        {
+            return Discount();
+        }
+        
         #endregion
     }
 }
